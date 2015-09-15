3009c7caa074ef4df8b6d0ca3699ef9ec16343a6
Faaez Ul Haq <faaez.ulhaq@gmail.com>
2015-09-15 16:51:07 -0400

    adding more functions

--- capiq.py
+++ capiq.py
@@ -6,17 +6,33 @@ class CapIQClient:
     _endpoint = 'https://sdk.gds.standardandpoors.com/gdssdk/rest/v2/clientservice.json'
     _headers = {'Content-Type': 'application/x-www-form-urlencoded','Accept-Encoding': 'gzip,deflate'}
 
-    def gdsp(self,tickers,mnemonics):
+    def gdsp(self,tickers,mnemonics,properties=None):
         req_array = []
+        if not properties:
+            properties = {}
         for ticker in tickers:
             for mnemonic in mnemonics:
-                req_array.append({"function":"GDSP","identifier":ticker,"mnemonic":mnemonic,"properties":{"PERIODTYPE":"IQ_FQ-2"}})
+                req_array.append({"function":"GDSP","identifier":ticker,"mnemonic":mnemonic,"properties":properties})
         req = {"inputRequests":req_array}
         r = requests.post(self._endpoint,headers=self._headers,data='inputRequests='+json.dumps(req),auth=HTTPBasicAuth(CAPIQ_USERNAME,CAPIQ_PASSWORD))
         return r.json()
 
-    def gdst(self,tickers,mnemonics,start_date,end_date=None,frequency='D'):
-        properties = {"STARTDATE":start_date,"FREQUENCY":frequency}
+    def gdspv(self,tickers,mnemonics,properties=None):
+        req_array = []
+        if not properties:
+            properties = {}
+        for ticker in tickers:
+            for mnemonic in mnemonics:
+                req_array.append({"function":"GDSPV","identifier":ticker,"mnemonic":mnemonic,"properties":properties})
+        req = {"inputRequests":req_array}
+        r = requests.post(self._endpoint,headers=self._headers,data='inputRequests='+json.dumps(req),auth=HTTPBasicAuth(CAPIQ_USERNAME,CAPIQ_PASSWORD))
+        return r.json()
+
+    def gdst(self,tickers,mnemonics,start_date,end_date=None,frequency='D',properties=None):
+        if not properties:
+            properties = {}
+        properties["FREQUENCY"] = frequency
+        properties["STARTDATE"] = start_date
         if end_date:
             properties["ENDDATE"] = end_date
 
@@ -27,6 +43,48 @@ class CapIQClient:
         req = {"inputRequests":req_array}
         r = requests.post(self._endpoint,headers=self._headers,data='inputRequests='+json.dumps(req),auth=HTTPBasicAuth(CAPIQ_USERNAME,CAPIQ_PASSWORD))
         return r.json()
+
+    def gdshe(self,tickers,mnemonics,start_date,end_date=None,properties=None):
+        if not properties:
+            properties = {}
+        properties["STARTDATE"] = start_date
+        if end_date:
+            properties["ENDDATE"] = end_date
+
+        req_array = []
+        for ticker in tickers:
+            for mnemonic in mnemonics:
+                req_array.append({"function":"GDSHE","identifier":ticker,"mnemonic":mnemonic,"properties":properties})
+        req = {"inputRequests":req_array}
+        r = requests.post(self._endpoint,headers=self._headers,data='inputRequests='+json.dumps(req),auth=HTTPBasicAuth(CAPIQ_USERNAME,CAPIQ_PASSWORD))
+        return r.json()
+
+    def gdshv(self,tickers,mnemonics,start_date,end_date=None,properties=None):
+        if not properties:
+            properties = {}
+        properties["STARTDATE"] = start_date
+        if end_date:
+            properties["ENDDATE"] = end_date
+
+        req_array = []
+        for ticker in tickers:
+            for mnemonic in mnemonics:
+                req_array.append({"function":"GDSHV","identifier":ticker,"mnemonic":mnemonic,"properties":properties})
+        req = {"inputRequests":req_array}
+        r = requests.post(self._endpoint,headers=self._headers,data='inputRequests='+json.dumps(req),auth=HTTPBasicAuth(CAPIQ_USERNAME,CAPIQ_PASSWORD))
+        return r.json()
+
+    def gdsg(self,tickers,group_mnemonics,properties=None):
+        if not properties:
+            properties = {}
+        req_array = []
+        for ticker in tickers:
+            for mnemonic in group_mnemonics:
+                req_array.append({"function":"GDSG","identifier":ticker,"mnemonic":mnemonic,"properties":properties})
+        req = {"inputRequests":req_array}
+        r = requests.post(self._endpoint,headers=self._headers,data='inputRequests='+json.dumps(req),auth=HTTPBasicAuth(CAPIQ_USERNAME,CAPIQ_PASSWORD))
+        return r.json()
+
 
 
 
--- tests.py
+++ tests.py
@@ -2,4 +2,5 @@ from capiq import CapIQClient
 
 if __name__ == '__main__':
     client = CapIQClient()
-    print client.gdst(['WMT:'],['IQ_TOTAL_REV'],start_date='11/12/2010',end_date='12/12/2011',frequency='Q')
+    print client.gdshe(['WMT:'],['IQ_CLOSEPRICE'],start_date='11/12/2010',end_date='11/18/2010')
+    # print client.gdsg(['WMT:'],['BASIC_SECURITY_DESCRIPTION'])
