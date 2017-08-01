9606a96883051b7474400f42d6f4310a7724c39b
Guy Mann <guydmann@gmail.com>
2017-08-01 16:49:26 -0400

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='capiq-python',
+    version='',
+    packages=['tests', 'tests.unit'],
+    url='',
+    license='',
+    author='gmann',
+    author_email='',
+    description=''
+)
